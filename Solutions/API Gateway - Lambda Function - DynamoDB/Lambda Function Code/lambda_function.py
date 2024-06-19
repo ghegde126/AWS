0bbc052c104581e8fddefdfe43f3cc188448a5b6
ghegde126 <ganapatihegde126@gmail.com>
2024-06-19 23:29:31 +0530

    Update Customer method

--- Solutions/API Gateway - Lambda Function - DynamoDB/Lambda Function Code/lambda_function.py
+++ Solutions/API Gateway - Lambda Function - DynamoDB/Lambda Function Code/lambda_function.py
@@ -12,70 +12,110 @@ dynamoDBClient = boto3.client('dynamodb')
 
 
 def lambda_handler(event, context):
-    if 'httpMethod','path' in event:
+    logger.info(event)
+    if 'httpMethod' in event:
+        return process_event(event['httpMethod'], event['path'], json.loads(event['body']))
 
     else:
-        return {
-            "StatusCode" : 400,
-            "headers" : {
-                "Content-Type": "application/json"
-            },
-            "body" : "Invalid request, request must include parameters httpMethod and path"
-        }
+        return responseMakerWithMessage(400, "Invalid request, request must include parameters httpMethod and path")
 
 
 def process_event(APIMethod, path, body):
-    if (APIMethod == 'GET' and path == 'healthCheck'):
-        return {
-            "StatusCode" : 200,
-            "headers" : {
-                "Content-Type": "application/json"
-            },
-            "body" : "Connection is working..."
-        }
+    print('inside process')
+    print(APIMethod)
+    print(path)
+    print(body)
+    if (APIMethod == 'GET' and path == '/healthCheck'):
+        return responseMakerWithMessage(200, "Connection is working...")
     
-    elif (APIMethod == 'POST' and path == 'createCustomer'):
+    elif 'CustomerId' not in body:
+        print('inside customer id not found')
+        return responseMakerWithMessage(400, "CustomerId key is not present in the request, please provide this key.")
+
+    elif (APIMethod == 'POST' and path == '/getCustomer'):
+        return getCustomer(body)
+
+    elif (APIMethod == 'POST' and path == '/createCustomer'):
+        print('inside create customer ')
+        return createCustomer(body)
+    
+    elif (APIMethod == 'POST' and path == '/updateCustomer'):
+        return updateCustomer(body)
+
+    elif (APIMethod == 'POST' and path == '/updateCustomerEmail'):
+        return updateCustomerEmail(body)
+    
+    else:
+        return responseMakerWithMessage(400, "Invalid Request")
+
 
 def createCustomer(eventBody):
-    if 'CustomerId' in eventBody:
-        try:
-            dynamoDBClient.put_item(
-                TableName = dynamoDB_table_name,
-                Item = eventBody
-            )
-        
-        except Exception as exception:
-            log.error(str(exception))
-
-
-
-    else:
-        log.info(CustomerId + ' is not present in table '+ dynamoDB_table_name)
-        return responseMakerWithMessage(400, "CustomerId key is not present in the request, please provide this key.")
+    print('here')
+    try:
+        dynamoDBClient.put_item(
+            TableName = dynamoDB_table_name,
+            Item = eventBody
+        )
+        print('done')
+        return responseMakerWithMessage(200, "{} saved successfully.".format(eventBody['CustomerId']))
     
+    except Exception as exception:
+        logger.error(str(exception))
+        return responseMakerWithMessage(400, "Unable to process the request")
+
+    
+def updateCustomer(eventBody):
+    try:
+        dynamoDBClient.put_item(
+            TableName = dynamoDB_table_name,
+            Item = eventBody
+        )
+        return responseMakerWithMessage(200, "{} updated successfully.".format(eventBody['CustomerId']))
+    
+    except Exception as exception:
+        logger.error(str(exception))
+        return responseMakerWithMessage(400, "Unable to process the update request")
+
+
+def updateCustomerEmail(eventBody):
+    try:
+        dynamoDBClient.update_item(
+            TableName = dynamoDB_table_name,
+            Key = { 'CustomerId' : eventBody['CustomerId'] },
+            ExpressionAttributeValues = { ':newEmail' : eventBody['c'] },
+            UpdateExpression = 'SET UpdateExpression = :newEmail'
+        )
+        return responseMakerWithMessage(200, "{} updated successfully.".format(eventBody['CustomerId']))
+
+    except Exception as exception:
+        logger.error(str(exception))
+        return responseMakerWithMessage(400, "Unable to process the update request")
+
+def getCustomerDetails(eventBody):
+    try:
+        response = dynamoDBClient.get_item(
+            TableName = dynamoDB_table_name,
+            Key = { 'CustomerId' : eventBody['CustomerId'] }
+            )
+
+        if len(response['Item']) != 0:
+            return responseMakerWithMessage(200, json.dumps(response['Item']))
+
+        else:
+            return responseMakerWithMessage(200, "Customer ID {} does not exists".format(eventBody['CustomerId']))
+
+    except Exception as exception:
+        logger.error(str(exception))
+        return responseMakerWithMessage(400, "Unable to process the update request")
+
+
+
 def responseMakerWithMessage(statusCode, message):
     return {
             "StatusCode" : statusCode,
             "headers" : {
                 "Content-Type": "application/json"
             },
-            "body" : message
+            "body" : json.dumps(message)
         }
 
-
-def setup_logging():
-    global log 
-    log = logging.getLogger()
-    log_levels = {'INFO':20, 'WARNING':30, 'ERROR':40}
-
-    if 'logging_level' in os.environ:
-        log_level = os.environ['logging_level'].upper()
-        if log_level in log_levels:
-            log.setLevel(log_levels[log_level])
-        else:
-            log.setLevel(log_levels['ERROR'])
-            logger.error('As logging level is not set to INFO, ERROR or WARNING, logging level is set to ERROR'))
-        
-    else:
-        log.setLevel(log_levels['ERROR'])
-    log.info('Logging setup is complete - set to log level ' + str(log.getEffectiveLevel()))
