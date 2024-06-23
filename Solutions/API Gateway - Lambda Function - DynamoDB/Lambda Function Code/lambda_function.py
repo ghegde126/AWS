adca5cf8970a4f131f1724a012b87cfb1872dd86
ghegde126 <ganapatihegde126@gmail.com>
2024-06-23 21:40:55 +0530

    CRUD functions update

--- Solutions/API Gateway - Lambda Function - DynamoDB/Lambda Function Code/lambda_function.py
+++ Solutions/API Gateway - Lambda Function - DynamoDB/Lambda Function Code/lambda_function.py
@@ -2,95 +2,64 @@ import json
 import boto3
 import logging
 import os
+import botocore
 
 logger = logging.getLogger()
 logger.setLevel(os.getenv('logging_level'))
 
 dynamoDB_table_name = os.getenv('dynamoDB_table_name')
-
 dynamoDBClient = boto3.client('dynamodb')
 
-
+# Main Function
 def lambda_handler(event, context):
-    logger.info(event)
+    print(event)
     if 'httpMethod' in event:
-        return process_event(event['httpMethod'], event['path'], json.loads(event['body']))
+        return process_event(event['httpMethod'], event['path'], event['body'])
 
     else:
         return responseMakerWithMessage(400, "Invalid request, request must include parameters httpMethod and path")
 
 
-def process_event(APIMethod, path, body):
-    print('inside process')
-    print(APIMethod)
-    print(path)
-    print(body)
+# Event processing function, based on API Request, it calls the sub functions
+def process_event(APIMethod, path, eventBody):
+
     if (APIMethod == 'GET' and path == '/healthCheck'):
         return responseMakerWithMessage(200, "Connection is working...")
-    
-    elif 'CustomerId' not in body:
-        print('inside customer id not found')
-        return responseMakerWithMessage(400, "CustomerId key is not present in the request, please provide this key.")
+
+    elif len(eventBody) == 0 or 'CustomerId' not in eventBody:
+        return responseMakerWithMessage(400, "Please provide necessary values in request body")
 
     elif (APIMethod == 'POST' and path == '/getCustomer'):
-        return getCustomer(body)
+        return getCustomerDetails(json.loads(jsonCleaner(eventBody)))
 
     elif (APIMethod == 'POST' and path == '/createCustomer'):
-        print('inside create customer ')
-        return createCustomer(body)
-    
+        return createCustomer(json.loads(jsonCleaner(eventBody)))
+
     elif (APIMethod == 'POST' and path == '/updateCustomer'):
-        return updateCustomer(body)
+        return updateCustomer(json.loads(jsonCleaner(eventBody)))
 
     elif (APIMethod == 'POST' and path == '/updateCustomerEmail'):
-        return updateCustomerEmail(body)
+        return updateCustomerEmail(json.loads(jsonCleaner(eventBody)))
     
     else:
         return responseMakerWithMessage(400, "Invalid Request")
 
 
+# This function puts a Customer Item in DynamoDB Table
 def createCustomer(eventBody):
-    print('here')
     try:
         dynamoDBClient.put_item(
             TableName = dynamoDB_table_name,
             Item = eventBody
         )
-        print('done')
-        return responseMakerWithMessage(200, "{} saved successfully.".format(eventBody['CustomerId']))
+        return responseMakerWithMessage(200, "{} saved successfully.".format(eventBody['CustomerId']['S']))
     
     except Exception as exception:
         logger.error(str(exception))
         return responseMakerWithMessage(400, "Unable to process the request")
-
     
-def updateCustomer(eventBody):
-    try:
-        dynamoDBClient.put_item(
-            TableName = dynamoDB_table_name,
-            Item = eventBody
-        )
-        return responseMakerWithMessage(200, "{} updated successfully.".format(eventBody['CustomerId']))
-    
-    except Exception as exception:
-        logger.error(str(exception))
-        return responseMakerWithMessage(400, "Unable to process the update request")
 
-
-def updateCustomerEmail(eventBody):
-    try:
-        dynamoDBClient.update_item(
-            TableName = dynamoDB_table_name,
-            Key = { 'CustomerId' : eventBody['CustomerId'] },
-            ExpressionAttributeValues = { ':newEmail' : eventBody['c'] },
-            UpdateExpression = 'SET UpdateExpression = :newEmail'
-        )
-        return responseMakerWithMessage(200, "{} updated successfully.".format(eventBody['CustomerId']))
-
-    except Exception as exception:
-        logger.error(str(exception))
-        return responseMakerWithMessage(400, "Unable to process the update request")
-
+# This function gets a Customer Item from DynamoDB Table
 def getCustomerDetails(eventBody):
     try:
         response = dynamoDBClient.get_item(
@@ -98,24 +67,66 @@ def getCustomerDetails(eventBody):
             Key = { 'CustomerId' : eventBody['CustomerId'] }
             )
 
-        if len(response['Item']) != 0:
+        if 'Item' in response:
             return responseMakerWithMessage(200, json.dumps(response['Item']))
 
         else:
-            return responseMakerWithMessage(200, "Customer ID {} does not exists".format(eventBody['CustomerId']))
+            return responseMakerWithMessage(200, "Customer ID {} does not exists".format(eventBody['CustomerId']['S']))
+    
+    except botocore.exceptions.ClientError as error:
+        if error.response['Error']['Code'] == 'ResourceNotFoundException':
+            return responseMakerWithMessage(200, "Customer ID {} does not exists".format(eventBody['CustomerId']['S']))
+        else:
+            return responseMakerWithMessage(400, "Unable to process the get customer details request botocore")
 
+    except Exception as exception:
+        logger.error(str(exception))
+        print(str(exception))
+        print(exception)
+        return responseMakerWithMessage(400, "Unable to process the get customer details request")
+    
+
+# This function updates entire Item
+def updateCustomer(eventBody):
+    try:
+        dynamoDBClient.put_item(
+            TableName = dynamoDB_table_name,
+            Item = eventBody
+        )
+        return responseMakerWithMessage(200, "{} updated successfully.".format(eventBody['CustomerId']['S']))
+    
     except Exception as exception:
         logger.error(str(exception))
         return responseMakerWithMessage(400, "Unable to process the update request")
 
 
+# This function updates only Email of Customer Item
+def updateCustomerEmail(eventBody):
+    try:
+        dynamoDBClient.update_item(
+            TableName = dynamoDB_table_name,
+            Key = { 'CustomerId' : eventBody['CustomerId']},
+            ExpressionAttributeValues = { ':newEmail' : eventBody['Email'] },
+            UpdateExpression = 'SET Email = :newEmail'
+        )
+        return responseMakerWithMessage(200, "{} updated successfully.".format(eventBody['CustomerId']['S']))
 
+    except Exception as exception:
+        logger.error(str(exception))
+        return responseMakerWithMessage(400, "Unable to process the update request")
+    
+
+# This function cleans the given JSON by removing some escape characters
+def jsonCleaner(data):
+    return data.replace("\\r\\n","")
+
+
+#This function creates a response 
 def responseMakerWithMessage(statusCode, message):
     return {
-            "StatusCode" : statusCode,
+            "statusCode" : statusCode,
             "headers" : {
                 "Content-Type": "application/json"
             },
-            "body" : json.dumps(message)
+            "body" : message
         }
-
